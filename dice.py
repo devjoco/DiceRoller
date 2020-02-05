fb659277303580b4e020f29ab4faf0a16b440d2c
devjoco <devonjoconnor@gmail.com>
2020-02-05 13:11:36 -0500

    parsing args for number of sides

--- dice.py
+++ dice.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import random
+import argparse
 
 def promptForInt(s):
     while True:
@@ -9,15 +10,26 @@ def promptForInt(s):
         except ValueError:
             print("You must enter an integer!")
 
-# Ask how many dice are being rolled
-num = promptForInt("How many dice are you rolling? (0 to stop) ")
+parser = argparse.ArgumentParser(description="Imitates the experience of rolling dice.")
+parser.add_argument("dice", nargs="?", help="how many dice are being rolled")
+parser.add_argument("-r", "--rolls", help="how many times the dice are rolled")
+parser.add_argument("-s", "--sides", help="how many sides each die will have",
+                    type=int, default=6)
+args = parser.parse_args()
 
-while(num != 0):
-    # Randomly calculate dice
-    for i in range(num):
-        thisDie = random.randint(1,6)
-        # Display dice
-        print(f"[{thisDie}] ",end='')
-    print()
 
+if args.sides == 0:
+    print("The dice must have at least 1 side!")
+else:
+    # Ask how many dice are being rolled
     num = promptForInt("How many dice are you rolling? (0 to stop) ")
+
+    while(num != 0):
+        # Randomly calculate dice
+        for i in range(num):
+            thisDie = random.randint(1,args.sides)
+            # Display dice
+            print(f"[{thisDie}] ",end='')
+        print()
+
+        num = promptForInt("How many dice are you rolling? (0 to stop) ")
